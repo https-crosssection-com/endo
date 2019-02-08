d53ec4ee78ad700af8105fe9b1cd818c53918495
unknown <asukaru2014@icloud.com>
2019-02-08 15:19:14 +0900

    クリックできるように修正残し

--- CrossSection/CrossSection-main-front.js
+++ CrossSection/CrossSection-main-front.js
@@ -1,17 +1,25 @@
 
- document.getElementById("right_bar").style.display="none";
- document.getElementById("main-display").style.display="none";
+//  document.getElementById("right_bar").style.display="none";
+//  document.getElementById("main-display").style.display="none";
 
-document.getElementById("lbl_btn_home").ondbclick=addEventListener{
+// document.getElementById("lbl_btn_home").ondbclick=addEventListener{
 
-}
-document.getElementById("lbl_btn_new").ondbclick=addEventListener{
-	document.getElementById("right_bar").style.display="block";
-    document.getElementById("main-display").style.display="block";
-}
-document.getElementById("lbl_btn_open").ondbclick=addEventListener{
+// }
+// document.getElementById("lbl_btn_new").ondbclick=addEventListener{
+// 	document.getElementById("right_bar").style.display="block";
+//     document.getElementById("main-display").style.display="block";
+// }
+// document.getElementById("lbl_btn_open").ondbclick=addEventListener{
 
-}
-document.getElementById("lbl_btn_docs").ondbclick=addEventListener{
+// }
+// document.getElementById("lbl_btn_docs").ondbclick=addEventListener{
 
-}
+// }
+
+			
+	console.log("sample");
+	$(function () {
+    $('#lbl_btn_home').click(function () {
+        $('.right_bar').show();
+    });
+});
