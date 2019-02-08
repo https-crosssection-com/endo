47d305481b42cdf473277181fbb8c3fcbe261480
takuma-ogawa <47406605+takuma-ogawa@users.noreply.github.com>
2019-02-09 00:34:15 +0900

    add card を押したときに回転するカードを表示する

---
+++ CrossSection-add-card-event/CrossSection-main-front.js
@@ -0,0 +1,26 @@
+
+
+
+			
+
+
+
+	$(function () {
+
+    $('#lbl_btn_new').click(function () {
+
+        $('.right_bar').show();
+        $('.main-display').show();
+
+    });
+
+});
+	$(function () {
+
+    $('#add_card').click(function () {
+
+        $('#rotate-div').show();
+
+    });
+
+});
