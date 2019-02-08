fad8f826b456081610686888a778ad2196772007
takuma-ogawa <47406605+takuma-ogawa@users.noreply.github.com>
2019-02-08 23:26:47 +0900

    Add files via upload
    newアイコンを押したときに背景画面とライトバーを出現させる挙動を完成

---
+++ CrossSection-new-move/CrossSection-main-front.js
@@ -0,0 +1,48 @@
+//  document.getElementById("right_bar").style.display="none";
+
+//  document.getElementById("main-display").style.display="none";
+
+
+
+// document.getElementById("lbl_btn_home").ondbclick=addEventListener{
+
+
+
+// }
+
+// document.getElementById("lbl_btn_new").ondbclick=addEventListener{
+
+// 	document.getElementById("right_bar").style.display="block";
+
+//     document.getElementById("main-display").style.display="block";
+
+// }
+
+// document.getElementById("lbl_btn_open").ondbclick=addEventListener{
+
+
+
+// }
+
+// document.getElementById("lbl_btn_docs").ondbclick=addEventListener{
+
+
+
+// }
+
+
+
+			
+
+
+
+	$(function () {
+
+    $('#lbl_btn_new').click(function () {
+
+        $('.right_bar').show();
+        $('.main-display').show();
+
+    });
+
+});
