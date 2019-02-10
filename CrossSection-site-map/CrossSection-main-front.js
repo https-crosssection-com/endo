058a74f2bd33999153d94d7a9e96402c07d25f4f
takuma-ogawa <47406605+takuma-ogawa@users.noreply.github.com>
2019-02-11 02:50:11 +0900

    HOMEボタンを押したときにサイトマップを表示させる

---
+++ CrossSection-site-map/CrossSection-main-front.js
@@ -0,0 +1,29 @@
+
+
+
+	$(function () {
+
+    $('#lbl_btn_new').click(function () {
+
+        $('#site-map').hide();
+        $('.right_bar').show();
+        $('.main-display').show();
+        $('#tab-control').show();
+        $('#tab-body').show();
+
+    });
+
+});
+	$(function () {
+
+    $('#add_card').click(function () {
+
+        $('#rotate-div').show();
+
+    });
+
+});
+
+
+
+    
---
+++ CrossSection-site-map/site-map.js
@@ -0,0 +1,13 @@
+$(function () {
+
+    $('#lbl_btn_home').click(function () {
+
+        $('#right_bar').hide();
+        $('#rotate-div').hide();
+        $('#tab-control').hide();
+        $('#tab-body').hide();
+        $('#site-map').show();
+
+    });
+
+});
