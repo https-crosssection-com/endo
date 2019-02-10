45fe5eb1d2bda30d3b91fa7c221f4f89bf843c43
takuma-ogawa <47406605+takuma-ogawa@users.noreply.github.com>
2019-02-10 22:26:29 +0900

    sheetタブの追加

---
+++ CrossSection-sheet-tab/CrossSection-main-front.js
@@ -0,0 +1,33 @@
+
+
+
+
+			
+
+
+
+	$(function () {
+
+    $('#lbl_btn_new').click(function () {
+
+        $('.right_bar').show();
+        $('.main-display').show();
+        $('#tab-control').show();
+        $('#tab-body').show();
+
+    });
+
+});
+	$(function () {
+
+    $('#add_card').click(function () {
+
+        $('#rotate-div').show();
+
+    });
+
+});
+
+
+
+    
